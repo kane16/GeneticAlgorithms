0894913443929237a774dc9d2ac33b6171e8d729
Łukasz Gumiński <lukasz.guminski629@gmail.com>
2019-03-02 18:15:11 +0100

    Added parameters class to set model.

---
+++ src/main/java/utilities/ModelParameters.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class ModelParameters {
+
+    public static double Pm = 0.22;
+    public static double Px = 0.33;
+    public static int pop_size = 100;
+    public static int gen = 10;
+
+}
